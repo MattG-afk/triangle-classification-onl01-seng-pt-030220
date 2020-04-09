4b271fdf596429df32877851f11f4fa2f1f426fd
Matt Gordon <bigmattsize21@gmail.com>
2020-04-09 23:57:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,8 +25,37 @@ class Triangle
 end
 
 class TriangleError < StandardError
-   "side must be greater than 0"
+   "size must be over 0"
 end
 
 
+class Triangle
+    attr_accessor :side_1, :side_2, :side_3
 
+    def initialize(side_1, side_2, side_3)
+      @side_1, @side_2, @side_3 = side_1, side_2, side_3
+    end
+
+    def illegal_triangle
+      if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+        raise TriangleError, "size must be over 0."
+      end
+    end
+
+    def kind
+      if @side_1 == @side_2 && @side_1 == @side_3
+        return :equilateral
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
+        return :isosceles
+      else
+        return :scalene
+     
+      end
+    end
+
+    
+
+    class TriangleError < StandardError
+    end
+
+end
